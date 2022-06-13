af5c91b7f5a1633ec5e65c838264b0823c041fea
Gaston Riveros <gastonbx2051@gmail.com>
2022-06-13 00:49:50 -0300

    ingresamos actualizacion  de archivos de python

---
+++ Python/Leccion2/Ejercicio 3.py
@@ -0,0 +1,14 @@
+# Ejercicio calcular la estacion del año
+mes = int(input("Digite eun mes del año ( 1 -12 ):"))
+estacion = None
+if mes == 1 or mes == 2 or mes == 3:
+    estacion = "Verano"
+elif mes == 4 or mes == 5 or mes == 6:
+    estacion = "Otoño"
+elif mes == 7 or mes == 8 or mes == 9:
+    estacion = "Invierno"
+elif mes == 10 or mes == 11 or mes == 12:
+    estacion == "Primavera"
+else:
+    estacion = "Error, no existe mes con ese numero digitado"
+print(f"Para el mes {mes} la estacion correspondiente es : {estacion}")
